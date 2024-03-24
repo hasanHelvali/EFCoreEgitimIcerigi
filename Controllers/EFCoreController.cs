b34b595944079c5ca9b074ce759697157bcf56e8
HasanHelvali <hasan.helvali.git@gmail.com>
2024-03-24 21:50:17 +0300

    VeriGuncelleme

--- Controllers/EFCoreController.cs
+++ Controllers/EFCoreController.cs
@@ -2,6 +2,9 @@
 using EFCoreEgitimi.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Globalization;
 
 namespace EFCoreEgitimi.Controllers
 {
@@ -84,7 +87,85 @@ namespace EFCoreEgitimi.Controllers
         }
 
         
+        public async void VeriGuncelleme()
+        {
+            #region Veri Nasıl Güncellenir
+            /*Verinin guncellenmesi demek davranıssal olarak eklenmis bir verinin elde edilip üzeirnde bazı degisiklikleirn yapılması demektir.
+            Dolayısıyla oncesinde veriyi elde etmem gerekir. */
+            Employee employee = await _context.Employees.FirstOrDefaultAsync(x => x.EmployeeId == 3);//3 id li ürün bana getirilir.
+            //Bu fonskiyon verilen sarta uyan ilk degeri dondurur, sarta uyan bir yapı yoksa null dondurur. Burada nesne elde edildi
+            employee.FirstName = "abc";
+            employee.Notes = "abc";
+            //Bu sekilde bazı guncellemeler yapılsın. Sımdi varolan nesne uzeirnde degisiklikler yapıldıgına gore ilgili yapı guncellenebilir.
+            await _context.SaveChangesAsync();
+            //Nesne uzeirnde yapılan degisiklikler efcore tarafından algılanır ve bu sekilde db ye yansıtılır.
+            #endregion
 
+            #region ChangeTracker nedir?
+            /*Bu bir takip mekanizmasıdır ve context uzeirnden gelen verilerin takıbınden sorumludur. Bu takip mekanizması vesilesiyle
+             bu context uzeirnden gelen verilerle ilgili islemler neticesinde update yahut delete sorgularının yapılıp yapılmayacagı 
+            anlasılır.
+            Az onceki veriler degistiginde efcore nasıl update yapacagını anladı? Bu mekanizma vesilesiyle anladı.
+            
+             Ozetle context ten gelen her bir veri kac tane olursa olsun default olarak ChangeTracker mekanizması ile kontrol edilir.*/
+            #endregion
+
+            #region Takip edilmeyen Nesneler Nasıl Guncellenir
+            Customer customer = new Customer
+            {
+                CustomerId = "abc",
+                CompanyName = "abc"
+            };
+            //Buradaki veri benim tarafımdan olusturuldu yani context uzeirnden gelmedi. Bu sebeple change tracker tarafından takip edilemiyor.
+            //Takip edilmeyen verileri gucnellemek icin ise EFCore tarafında Update fonksiyonu gelistirilmiştir.
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+            /*Burada abc id li bir veri var ve bu veri dbde varsa ilgili degerlerini benim veridigm gibi guncellenmesini istiyorum.
+             Bu yuzden once update kullanıp daha sonra change tracker i cagırmıs oldum.
+            Update fonksiyonunu kullanabilmem icin ilgili nesnede id nin verilmesi tabii ki gerekir.*/
+
+            #endregion
+
+            #region EntityState Nedir
+            /*Bir Entity insance inin duurmunu ifade eden bir referanstır.
+             Bir entity instance i demek db de bir tablonun icindeki veriyte yani satıra karsılık gelen bir nesne demektir.*/
+            Customer customer1 = new Customer();
+            var durum = _context.Entry(customer1).State;//Bir entity nin state i bu seilde elde edilir. Bu state ChangeTracker ile elde edilir.
+            /*Burada durum üzerine detached degeri gelir. Yani suan bu nesne uzerinde bir degisiklik yapılmamıs haldedir. ChangeTracker tarafından takipsizdir. 
+            Lakin ben bu nesneyi db ye insert edersem state inin added oldugunu gorebilirim. 
+            Eger bu nesneyi guncellemeye tabii tutarsam state inin modified oldugunu gorebilirim. 
+            Bu degerleri gormemin nedeni bu islemler sonucunda nesnenin change tracker ile takibe alınmasıdır. 
+            */
+            #endregion
+
+            #region Efcore Acısından bir verinin guncellenmesi gerektigi nasıl anlasılır.
+            var value = await _context.Customers.FirstOrDefaultAsync(x => x.CustomerId == "abc");
+            Console.WriteLine(_context.Entry(value).State);//unchanged degeri gelir. Yani takip altında fakat degisiklik yok
+            value.ContactTitle = "djlajlad";//Bir degisiklik yapıldı.
+            Console.WriteLine(_context.Entry(value).State);//modified degeri gelir. Yani nesnede bir degisiklik oldu demektir.
+            await _context.SaveChangesAsync();//db ye aktarım yapıldı. Butun degisiklikler kaydedildi.
+            Console.WriteLine(_context.Entry(value).State);//unchanged degeri gelir. Cunku butun degisiklikler db ye aktarılmıstır.
+            #endregion
+
+            #region Birden fazla veri guncellerken nelere dikkat edilmesi gerekir?
+            /*Soru aslında sudur: Her guncellemeden sonra mı SaveChanges cagrılır yoksa butun guncellemeler yapıldıktan sonra mı cagrılır?
+             Bu sorunun cevabı cogu zaman aynıdır. Fazla transaction olusmasın diye en sonra saceChanges cagrılması daha iyidir. Performansı artırır.*/
+
+            var values= await _context.Customers.ToListAsync();//Bu bir select*from sorgusudur.
+            foreach (var item in values)
+            {
+                item.CompanyName += "*";//her name ozelliginin yanına bir * ekledim. Yani entity yi degistiriyorum.
+                
+                //await _context.SaveChangesAsync();//Eger ben saveChanges fonk. burada cagırırsam her dongude bir transaction olusturmus olurum.
+                //1000 urun musteri varsa 1000 kez execute islemi yapılıyor demektir.
+            }
+            await _context.SaveChangesAsync();
+            /*Lakin SaveChanges fonksiyonunu burada cagırırsam 1000 urun varsa ve bunlar degisiyorsa bir tane transaction ile bu veriler db ye
+            aktarılmıs olur. Bu muazzam bir maliyet tasarrufu demektir. Tek sorgu icine butun guncellenmesi gereken nesneler alınır ve oyle 
+            execute  yapılır. Bu yuzden tasarruf saglanır.*/
+            #endregion
+
+        }
 
     }
 }
